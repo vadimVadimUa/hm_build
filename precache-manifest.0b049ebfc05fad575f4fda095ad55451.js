509f84485e9c95ad68651b5e097b301e08c8fa72
VadimVadimUa <puet12vadim@gmail.com>
2020-03-10 01:02:29 +0200

    Init

---
+++ precache-manifest.0b049ebfc05fad575f4fda095ad55451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd1becc50f1e0d07a16a6a4622b5097",
+    "url": "/hm_build/index.html"
+  },
+  {
+    "revision": "a702632dde40b1e77b82",
+    "url": "/hm_build/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ac0b9830f0c3acb46a44",
+    "url": "/hm_build/static/css/main.8ca9c717.chunk.css"
+  },
+  {
+    "revision": "a702632dde40b1e77b82",
+    "url": "/hm_build/static/js/2.f145856b.chunk.js"
+  },
+  {
+    "revision": "ac0b9830f0c3acb46a44",
+    "url": "/hm_build/static/js/main.f636c4f0.chunk.js"
+  },
+  {
+    "revision": "e7950dd8435f1e7526b1",
+    "url": "/hm_build/static/js/runtime-main.114c8817.js"
+  }
+]);
